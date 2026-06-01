bc2c8b25a1a62973e5357af91218e9d781dd4911
gskaboagye <gskaboagye@gmail.com>
2026-06-01 21:56:26 +0000

    run

--- src/config/passport.js
+++ src/config/passport.js
@@ -6,7 +6,10 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "/auth/github/callback",
+      callbackURL:
+        process.env.NODE_ENV === "production"
+          ? "https://sefa-tees-api.onrender.com/auth/github/callback"
+          : "http://localhost:3000/auth/github/callback",
     },
     (accessToken, refreshToken, profile, done) => {
       return done(null, profile);
